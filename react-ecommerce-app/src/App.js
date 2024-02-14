0283a4f311d2154f0b2aa04e33f4fd1347335ac9
macdonald34 <+>
2024-02-14 20:10:05 +0300

    Completed Project

---
+++ react-ecommerce-app/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/products')
+      .then(response => response.json())
+      .then(data => setProducts(data));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>E-commerce App</h1>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            {product.name} - ${product.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
